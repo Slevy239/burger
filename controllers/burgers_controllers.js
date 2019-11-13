3d69f46620b4e473c75f5340ecb120c02975f259
Sam Levy <slevy239@gmail.com>
2019-11-13 14:23:01 -0500

    get post working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,29 +6,38 @@ var burger = require("../models/burger.js");
 
 
 router.get("/", function (req, res) {
-  res.render("index")
-});
-
-
-router.get("/burgers", function (req, res) {
-  burger.all(function (burgerData) {
-    res.render("index", { burger_data: burgerData })
+  burger.all(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    // console.log(hbsObject)
+    res.render("index", hbsObject)
   });
 });
+
+
+
+// });
 
 router.post("/burgers/create", function (req, res) {
   burger.create(req.body.burger_name, function (result) {
-    console.log(result)
-    res.redirect("/");
-  })
+    // console.log(result)
+    // res.redirect("/");
+    res.redirect("/")
+  });
 });
 
-router.put("/burgers/:id", function (req, res) {
-  burger.update(req.params.id, function (result) {
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
     console.log(result);
+    // Send back response and let page reload from .then in Ajax
     res.sendStatus(200);
   });
 });
+
+
 
 
 // Export routes for server.js to use.
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
     
-  $(".devour-form").on("submit", function(event) {
+  $(".submit").on("submit", function(event) {
     event.preventDefault();
 
-    var newBurger = {
-      name: $("#name").val(),
-      devoured: 0
-    }
+    var burger_id = $(this).children(".burger_id").val();
 
-    console.log(newBurger);
-    // var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id);
     $.ajax({
       method: "PUT",
-      url: "/burgers/" + newBurger
+      url: "/burgers/" + burger_id
     }).then(function(data) {
       // reload page to display devoured burger in proper column
       location.reload();
