55f21d84f1ff2087be0e0bf7a7d3fb179488db20
Sam Levy <slevy239@gmail.com>
2019-11-13 19:19:10 -0500

    css

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-var express = require("express");
+const express = require('express');
+const path = require('path');
 
-var PORT = process.env.PORT || 8080;
-var app = express();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use('/public', express.static(path.join(__dirname + '/public')));
 
 // Parse application body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers");
+// Import routes and give the server access
+const routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
-// Start our server so that it can begin listening to client requests.
+
+// Start the sever so that it can begin listening to client side requests
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+
+    console.log("Server listening on: http://localhost:" + PORT);
+
 });
